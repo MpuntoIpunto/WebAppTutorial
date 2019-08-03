97cdb725984135984c7c228fcc3a23391aaa0f73
Markus Igel <migel@dataahead.de>
2019-08-03 23:45:49 +0200

    Bugfix where editHandler did not create new page if not exist, added some static html using css and js: microphne input does not work - only works if server is started and the index.html is called directly through file into browser

--- wiki.go
+++ wiki.go
@@ -54,7 +54,6 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
-		return
 	}
 	renderTemplate(w, "edit", p)
 	log.Println(title + " was loaded!")
@@ -73,6 +72,18 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(title + " was loaded!")
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
+
+// func indexHandler(w http.ResponseWriter, r *http.Request, title string) {
+// 	p := &Page{Title: title}
+// 	err := templates.ExecuteTemplate(w, "index.html", p)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 	}
+// }
+
 func main() {
 	/* First part of tutorial, just write to a file an load it again
 	p1 := &Page{Title: "Test Page", Body: []byte("This is a sample Page.")}
@@ -84,5 +95,10 @@ func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/index/", indexHandler)
+	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/fft/", http.StripPrefix("/fft/", http.FileServer(http.Dir("fft"))))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
